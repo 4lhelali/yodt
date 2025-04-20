ba919839cbb3f522a47a8422e50976c25e4a3e75
Mohammed <106600096+4lhelali@users.noreply.github.com>
2025-04-20 10:06:06 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-// import "./src/env.js";
+ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
-    domains: ["image.alhaymex.com", "lh3.googleusercontent.com", "utfs.io"],
+    domains: ["image.alhaymex.com", "lh3.googleusercontent.com", "utfs.io" , 'upload.wikimedia.org'],
   },
 };
 
